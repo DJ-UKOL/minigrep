52235fb8077c3b455579371982916f68145c0025
Admin <dj-ukol@yandex.ru>
2024-08-18 22:36:50 +0500

    Grouping Configuration Values. Группировка конфигурационных переменных.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ use std::{env, fs};
 fn main() {
     // Позволяем программе читать любые переданные ей аргументы командной строки,
     let args: Vec<String> = env::args().collect();  // а затем собирать значения в вектор.
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)    // открывает файл и возвращает содержимое файла
+    let contents = fs::read_to_string(config.file_path)    // открывает файл и возвращает содержимое файла
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,      // Строка, которую мы ищем
+    file_path: String,  // Путь к файлу
+}
+fn parse_config(args: &[String]) -> Config {
     // &args[0] - имя программы ("target/debug/minigrep")
-    let query = &args[1];       // Строка, которую мы ищем
-    let file_path = &args[2];   // Путь к файлу
+    let query = args[1].clone();       // .clone() делает полную копию данных для экземпляра Config для владения
+    let file_path = args[2].clone();
 
-    (query, file_path)
+    Config { query, file_path }      // возвращаем структуру
 }
