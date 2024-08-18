ef25ffb8d2cf85813999b9249dbcc72f6f1b77da
Admin <dj-ukol@yandex.ru>
2024-08-18 21:17:11 +0500

    Saving the Argument Values in Variables. Сохранения значений аргументов в переменные.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ use std::env;
 fn main() {
     // Позволяем программе читать любые переданные ей аргументы командной строки,
     let args: Vec<String> = env::args().collect();  // а затем собирать значения в вектор.
-    dbg!(args);                             //  печатаем вектор с помощью отладочного макроса
+
+    // &args[0] - имя программы ("target/debug/minigrep")
+    let query = &args[1];       // Строка, которую мы ищем
+    let file_path = &args[2];   // Путь к файлу
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
