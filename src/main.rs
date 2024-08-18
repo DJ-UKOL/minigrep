1d2dea5e044d9d4009bbafb6ecafa242104d9f29
Admin <dj-ukol@yandex.ru>
2024-08-18 22:44:36 +0500

    Creating a Constructor for Config. Создание конструктора для структуры Config.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     // Позволяем программе читать любые переданные ей аргументы командной строки,
     let args: Vec<String> = env::args().collect();  // а затем собирать значения в вектор.
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -18,10 +18,15 @@ struct Config {
     query: String,      // Строка, которую мы ищем
     file_path: String,  // Путь к файлу
 }
-fn parse_config(args: &[String]) -> Config {
-    // &args[0] - имя программы ("target/debug/minigrep")
-    let query = args[1].clone();       // .clone() делает полную копию данных для экземпляра Config для владения
-    let file_path = args[2].clone();
 
-    Config { query, file_path }      // возвращаем структуру
+impl Config {
+
+    // Конструктор для структуры Config
+    fn new(args: &[String]) -> Config {
+        // &args[0] - имя программы ("target/debug/minigrep")
+        let query = args[1].clone();       // .clone() делает полную копию данных для экземпляра Config для владения
+        let file_path = args[2].clone();
+
+        Config { query, file_path }      // возвращаем структуру
+    }
 }
