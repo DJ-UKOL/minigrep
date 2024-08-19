8f02e364bcf8c9bcd9d64255f22457f4d429bf9e
Admin <dj-ukol@yandex.ru>
2024-08-19 08:15:33 +0500

    Fixing the Error Handling. Исправление ошибок обработки. Improving the Error Message. Улучшение сообщения об ошибке. Returning a Result Instead of Calling panic! Возвращение Result вместо вызова panic! Calling Config::build and Handling Errors. Вызов Config::build и обработка ошибок.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     // Позволяем программе читать любые переданные ей аргументы командной строки,
     let args: Vec<String> = env::args().collect();  // а затем собирать значения в вектор.
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);           // немедленно остановит программу и вернёт номер,
+                                         // который был передан в качестве кода состояния выхода.
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -22,11 +26,15 @@ struct Config {
 impl Config {
 
     // Конструктор для структуры Config
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {     // 'static - статическое время жизни которое равно времени выполнения программы
+        // нужно проверять, что срез достаточно длинный, перед попыткой доступа по индексам 1 и 2
+        if args.len() < 3 {
+            return Err("not enough argument");  // Если меньше 3, то возвращаем ошибку
+        }
         // &args[0] - имя программы ("target/debug/minigrep")
         let query = args[1].clone();       // .clone() делает полную копию данных для экземпляра Config для владения
         let file_path = args[2].clone();
 
-        Config { query, file_path }      // возвращаем структуру
+        Ok(Config { query, file_path })
     }
 }
